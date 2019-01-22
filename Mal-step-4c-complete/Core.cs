a61f750c9a1b59f974ca32af3c531eeae48bd8f4
KineticLensman <john.kent.42@googlemail.com>
2019-01-22 22:00:48 +0000

    Created via upload

---
+++ Mal-step-4c-complete/Core.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Mal.Types;
+
+// Useful notes https://docs.microsoft.com/en-us/dotnet/csharp/lambda-expressions
+
+// The builtin functions available from within Mal. They are written as lambda
+// expressions that take a list of args. These builtins are added to the Mal Env at 
+// start-up and may be extended by programmes that call def.
+
+namespace Mal
+{
+    class Core
+    {
+        // Some constants.
+        public static MalTrue malTrue = new MalTrue();
+        public static MalFalse malFalse = new MalFalse();
+        public static MalNil malNil = new MalNil();
+
+        //------------------ Utilities for Numbers and numeric ops -------------------------------------------
+        // C#-Mal only allows integers and thus avoids a lot of complexity. JKL handles doubles but
+        // doesn't have a separate int type to keep things simple.
+        // Also unlike C#-Mal, numeric args can have arg lists of variable  length, e.g. (+ 1 2 3).
+
+        // Numeric functions that can be applied to lists of numbers, used to handle special cases.
+        protected enum NumericOp { Plus, Multiply, Subtract, Divide };
+
+        static protected MalNum ProcessNumbers(MalList args, NumericOp op)
+        {
+            double result = 0;
+
+            // Initiate the calculation with the first arg.
+            switch (args[0])
+            {
+                case MalNum num:
+                    result = (double)num;
+                    // Handle the special case of (- 1).
+                    if (args.Count() == 1 && op == NumericOp.Subtract)
+                    {
+                        double negative = (double)num;
+                        return new MalNum(-negative);
+                    }
+                    break;
+
+                default:
+                    // Should have already detected this but just in case.
+                    throw new MalInternalError("Non-number while calculating numbers: '" + args[0].ToString(true));
+            }
+            double divTest = 0;
+
+            // Now apply the op to the remaining args.
+            for (var i = 1; i < args.Count(); i++)
+            {
+                switch (args[i])
+                {
+
+                    case MalNum num:
+                        switch (op)
+                        {
+                            case NumericOp.Plus:
+                                result += (double)num;
+                                break;
+                            case NumericOp.Multiply:
+                                result *= (double)num;
+                                break;
+                            case NumericOp.Subtract:
+                                result -= (double)num;
+                                break;
+                            case NumericOp.Divide:
+                                divTest = (double)num;
+                                if (i > 0 && divTest == 0)
+                                {
+                                    throw new MalEvalError("Can't divide by zero");
+                                }
+                                result /= divTest;
+                                break;
+                        }
+                        break;
+                    default:
+                        throw new MalInternalError("Non-number while calculating numbers: '" + args[i].ToString(true));
+                }
+            }
+            return new MalNum(result);
+        }
+
+        //------------------ Builtins for Numbers and numeric ops -------------------------------------------
+
+        public static MalFunc BuiltInAdd = new MalFunc(args =>
+        {
+            return ProcessNumbers(args, NumericOp.Plus);
+        });
+
+        public static MalFunc BuiltInMultiply = new MalFunc(args =>
+        {
+            return ProcessNumbers(args, NumericOp.Multiply);
+        });
+
+        public static MalFunc BuiltInSubtract = new MalFunc(args =>
+        {
+            return ProcessNumbers(args, NumericOp.Subtract);
+        });
+
+        public static MalFunc BuiltInDivide = new MalFunc(args =>
+        {
+
+            return ProcessNumbers(args, NumericOp.Divide);
+        });
+
+        //------------------ Builtins for List manipulation -------------------------------------------
+
+        // Make a list containing the supplied args (already eval'd). Returns an empty list, not nil, if 
+        // there are no args.
+        public static MalFunc BuiltinList = new MalFunc(args =>
+        {
+            MalList newList = new MalList();
+
+            for (var i = 0; i < args.Count(); i++)
+            {
+                newList.Add(args[i]);
+            }
+            return newList;
+        });
+
+        // Return true if first param is a List, otherwise false.
+        public static MalFunc BuiltinIsList = new MalFunc(args =>
+        {
+            if (args.Count() != 1)
+            {
+                throw new MalEvalError("list? expects a single argument but got " + args.Count());
+            }
+            if (args[0] is MalList)
+            {
+                return malTrue;
+            }
+            else
+            {
+                return malFalse;
+            }
+        });
+
+        public static MalFunc BuiltinEmpty = new MalFunc(args =>
+        {
+            if (args.Count() != 1)
+            {
+                throw new MalEvalError("empty? expects a single argument but got " + args.Count());
+            }
+            if (args[0] is MalList testList)
+            {
+                if (testList.Count() == 0)
+                {
+                    return malTrue;
+                }
+                else
+                {
+                    return malFalse;
+                }
+            }
+            else
+            {
+                throw new MalEvalError("empty? expects a single list");
+            }
+        });
+
+        public static MalFunc BuiltinCount = new MalFunc(args =>
+        {
+            if (args.Count() != 1)
+            {
+                throw new MalEvalError("count expects a single argument");
+            }
+            if (args[0] is MalList testList)
+            {
+                return new MalNum(testList.Count());
+            }
+            else if (args[0] is MalNil)
+            {
+                return new MalNum(0);
+            }
+            else
+            {
+                throw new MalEvalError("count expects a list or nil");
+
+            }
+        });
+
+        //------------------ Builtins for boolean comparisons -------------------------------------------
+
+        public static MalFunc BuiltinEQ = new MalFunc(args =>
+        {
+            if (args.Count() != 2)
+            {
+                throw new MalEvalError("Comparison operator expects two arguments but got " + args.Count());
+            }
+            return MalVal.EQ(args[0], args[1]);
+        });
+
+        // Helper for functions that expect two numeric args.
+        // TODO - handle monotonic argument lists
+        private static void CheckComparisonParams(MalList args)
+        {
+            if (args.Count() != 2)
+            {
+                throw new MalEvalError("Comparison operator expects two arguments but got " + args.Count());
+            }
+            if (!((object)args[0] is MalNum))
+            {
+                throw new MalEvalError("Number expected but got '" + args[0].ToString(true) + "'");
+            }
+            if (!((object)args[1] is MalNum))
+            {
+                throw new MalEvalError("Number expected but got '" + args[1].ToString(true) + "'");
+            }
+        }
+
+        public static MalFunc BuiltinLT = new MalFunc(args =>
+        {
+            CheckComparisonParams(args);
+            if ((MalNum)args[0] < (MalNum)args[1])
+            {
+                return malTrue;
+            }
+            return malFalse;
+        });
+
+        public static MalFunc BuiltinLToE = new MalFunc(args =>
+        {
+            CheckComparisonParams(args);
+            if ((MalNum)args[0] <= (MalNum)args[1])
+            {
+                return malTrue;
+            }
+            return malFalse;
+        });
+
+        public static MalFunc BuiltinGT = new MalFunc(args =>
+        {
+            CheckComparisonParams(args);
+            if ((MalNum)args[0] > (MalNum)args[1])
+            {
+                return malTrue;
+            }
+            return malFalse;
+        });
+
+        public static MalFunc BuiltinGToE = new MalFunc(args =>
+        {
+            CheckComparisonParams(args);
+            if ((MalNum)args[0] >= (MalNum)args[1])
+            {
+                return malTrue;
+            }
+            return malFalse;
+        });
+
+        //------------------ Builtins for printing -------------------------------------------
+
+        // Calls pr_str on each argument with print_readably set to true, joins the results
+        // with " " and returns the new string.
+        public static MalFunc BuiltinPrStr = new MalFunc(args =>
+        {
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < args.Count(); i++)
+            {
+                if( i > 0)
+                {
+                    sb.Append(" ");
+                }
+                sb.Append(Printer.pr_str(args[i], true));
+            }
+            return new MalString(sb.ToString());
+        });
+
+        // calls pr_str on each argument with print_readably set to false, concatenates the
+        // results together ("" separator), and returns the new string.
+        public static MalFunc BuiltinStr = new MalFunc(args =>
+        {
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < args.Count(); i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append(" ");
+                }
+                sb.Append(Printer.pr_str(args[i], false));
+            }
+            return new MalString(sb.ToString());
+        });
+
+        // Calls pr_str on each argument with print_readably set to true, joins the results
+        // with " ", prints the string to the screen and then returns nil.
+        public static MalFunc BuiltinPrn = new MalFunc(args =>
+        {
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < args.Count(); i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append(" ");
+                }
+                sb.Append(Printer.pr_str(args[i], true));
+            }
+            Console.WriteLine(sb.ToString());
+            return malNil;
+        });
+
+        // Calls pr_str on each argument with print_readably set to false, joins the results
+        // with " ", prints the string to the screen and then returns nil.
+        public static MalFunc BuiltinPrintLn = new MalFunc(args =>
+        {
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < args.Count(); i++)
+            {
+                if (i > 0)
+                {
+                    sb.Append(" ");
+                }
+                sb.Append(Printer.pr_str(args[i], false));
+            }
+            Console.WriteLine(sb.ToString());
+            return malNil;
+        });
+
+    }
+}
---
+++ Mal-step-4c-complete/Types.cs
@@ -0,0 +1,504 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Mal;
+//using static Mal.Types;
+using static Mal.Env;
+using static Mal.Core;
+
+namespace Mal
+{
+    // The various types (symbols, lists, numbers, etc) understood by Mal.
+    public class Types
+    {
+        // TODO - see C# docs = need to add all four exception constructors.
+        // Mal exceptions.
+        public class MalException : Exception
+        {
+            public MalException(): base("Unknown MAL exception")
+            {
+                // Nothing more to construct here.
+            }
+
+            public MalException(string message) : base(message)
+            {
+                // Nothing more to construct here.
+            }
+        }
+
+        public class MalParseError : MalException
+        {
+            public MalParseError(string message) : base("Read error: " + message) { }
+        }
+
+        public class MalEvalError : MalException
+        {
+            public MalEvalError(string message) : base("Eval error: " + message) { }
+        }
+
+        public class MalInternalError : MalException
+        {
+            public MalInternalError(string message) : base("Internal error: " + message) { }
+        }
+        
+        // Base class for the Mal type system.
+        public abstract class MalVal
+        {
+            public virtual string ToString(bool printReadably)
+            {
+                // Default is the regular behaviour.
+                return this.ToString();
+            }
+
+            // Support the built-in '=' function. False until explicitly proven otherwise.
+            public static MalVal EQ (MalVal a, MalVal b)
+            {
+                if (a.GetType() != b.GetType())
+                {
+                    // TODO - allow equality comparisons between ints and floats.
+                    return malFalse;
+                }
+                // If here, they are of the same Mal type. Do they have the same value?
+                switch ((object) a)
+                {
+                    case MalSym aSym:
+                        if (aSym.getName() == ((MalSym)b).getName()) { return malTrue; }
+                        break;
+
+                    case MalNum aNumk:
+                        if (aNumk == ((MalNum)b)) { return malTrue; }
+                        break;
+
+                    case MalString aString:
+                        if (aString.ToString() == ((MalString)b).ToString()) { return malTrue; }
+                        break;
+
+                    case MalKeyword aKeyWord:
+                        if (aKeyWord.ToString() == ((MalKeyword)b).ToString()) { return malTrue; }
+                        break;
+
+                    case MalSeq aSeq:
+                        // Sequences must be the same length, and each element must be the same.
+                        MalSeq bSeq = (MalSeq)b;
+                        if (aSeq.Count () != bSeq.Count())
+                        {
+                            // They are not of equal length.
+                            return malFalse;
+                        }
+                        for (var i = 0; i < aSeq.Count(); i++)
+                        {
+                            // At least one of the elements is not equal.
+                            if (EQ(aSeq[i], bSeq[i]) == malFalse)
+                            {
+                                return malFalse;
+                            }
+                        }
+                        return malTrue;
+
+                    default:
+                        throw new MalInternalError("Can't yet compare '" + a.GetType() + "' with '" + b.GetType() + "'");
+                }
+                return malFalse;
+            }
+        }
+
+        // Base class for lists, vectors and other sequences.
+        // JK approach. In the C#-Mal reference, Vector is derived from List, not Sequence.
+        public abstract class MalSeq : MalVal
+        {
+            // They are all lists under the skin.
+            protected List<MalVal> MyElements;
+
+            protected MalSeq()
+            {
+                MyElements = new List<MalVal>();
+            }
+
+            protected string PrintSeq(bool printReadably, char startChar, char endChar)
+            {
+                StringBuilder sb = new StringBuilder();
+                bool middle = false;
+
+                sb.Append(startChar);
+                foreach (MalVal mv in MyElements)
+                {
+                    if( ! middle)
+                    {
+                        middle = true;
+                    }
+                    else
+                    {
+                        sb.Append(" ");
+                    }
+                    sb.Append(mv.ToString(printReadably));
+                }
+                sb.Append(endChar);
+
+                return sb.ToString();
+            }
+
+            public void Add(MalVal newVal)
+            {
+                MyElements.Add(newVal);
+            }
+
+            public int Count()
+            {
+                // TODO - check whether this is correct for a hashmap. If not, override.
+                return MyElements.Count;
+            }
+
+            // Return the first element. Equivalent to CL's (CAR..).
+            public MalVal First()
+            {
+                if(MyElements.Count > 0)
+                {
+                    return MyElements[0];
+                }
+                else
+                {
+                    return malNil;
+                }
+            }
+
+            // Return the tail of the list. Equivalent to CL's (CDR..).
+            public MalList Rest()
+            {
+                if (MyElements.Count > 0)
+                {
+                    // We return a MalList, not a List<MalVal>.
+                    MalList newList = new MalList();
+
+                    foreach( var element in MyElements.GetRange(1, MyElements.Count - 1))
+                    {
+                        newList.Add(element);
+                    }
+                    return newList;
+                }
+                else
+                {
+                    return new MalList();
+                }
+            }
+
+            public MalVal Get(int index)
+            {
+                if( index < 0 || index >= MyElements.Count)
+                {
+                    throw new MalEvalError("Attempt to access element " + index.ToString() + " of a list with " + MyElements.Count.ToString() + "elements");
+                }
+                return MyElements[index];
+            }
+
+            // Indexer that allows range checked array-style access.
+            public MalVal this[int index]
+            {
+                get
+                {
+                    if (index < 0 || index >= MyElements.Count)
+                    {
+                        throw new MalEvalError("Attempt to access element " + index.ToString() + " of a sequence with " + MyElements.Count.ToString() + "elements");
+                    }
+                    return MyElements[index];
+                }
+                set
+                {
+                    if (index < 0 || index >= MyElements.Count)
+                    {
+                        throw new MalEvalError("Attempt to access element " + index.ToString() + " of a sequence with " + MyElements.Count.ToString() + "elements");
+                    }
+                    MyElements[index] = value;
+                }
+            }
+        }
+
+        // Derived class for Mal lists
+        public class MalList : MalSeq
+        {
+            public override string ToString(bool printReadably)
+            {
+                return PrintSeq(printReadably, '(', ')');
+            }
+
+            // Default constuctor - for an empty list.
+            public MalList()
+            {
+                // Base class does the heavy lifting.
+            }
+
+            // Constructor for internal use, e.g. when read_form handles a MalQuote.
+            public MalList(MalVal firstElement, MalVal secondElement)
+            {
+                // TODO - explore var arg lists if required.
+                Add(firstElement);
+                Add(secondElement);
+            }
+        }
+
+        // Derived class for Mal vectors
+        public class MalVector : MalSeq
+        {
+            public override string ToString(bool printReadably)
+            {
+                return PrintSeq(printReadably, '[', ']');
+            }
+        }
+
+        // Derived class for Mal vectors
+        public class MalHashMap : MalSeq
+        {
+            // TODO - the reader should actually be able to construct these
+            public override string ToString(bool printReadably)
+            {
+                return PrintSeq(printReadably, '{', '}');
+            }
+        }
+
+        // Derived class for Mal symbols
+        public class MalSym : MalVal
+        {
+            private readonly string MySymbol;
+
+            public MalSym(string symbol)
+            {
+                MySymbol = symbol;
+            }
+
+            public override string ToString(bool printReadably)
+            {
+                return MySymbol;
+            }
+
+            public string getName() { return MySymbol; }
+
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+
+            public static bool operator ==(MalSym a, MalSym b)
+            {
+                return a.MySymbol == b.MySymbol;
+            }
+            public static bool operator !=(MalSym a, MalSym b)
+            {
+                return a.MySymbol != b.MySymbol;
+            }
+        }
+
+        // Derived class for Mal numbers. The C#-Mal reference only handles
+        // integers but JKL uses (and can parse) doubles. I used to have
+        // separate int and float classes but these complicated lot of ops.
+        public class MalNum : MalVal
+        {
+            double myVal = 0;
+
+            public MalNum(double num)
+            {
+                myVal = num;
+            }
+
+            public override string ToString(bool printReadably)
+            {
+                return myVal.ToString();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+
+            public static explicit operator int(MalNum n)
+            {
+                return (int)n.myVal;
+            }
+            public static explicit operator float(MalNum n)
+            {
+                return (float)n.myVal;
+            }
+            public static explicit operator double(MalNum n)
+            {
+                return n.myVal;
+            }
+
+            public static MalNum operator +(MalNum a, MalNum b)
+            {
+                return new MalNum(a.myVal + b.myVal);
+            }
+
+            public static bool operator ==(MalNum a, MalNum b)
+            {
+                return a.myVal == b.myVal;
+            }
+            public static bool operator !=(MalNum a, MalNum b)
+            {
+                return a.myVal != b.myVal;
+            }
+
+            public static bool operator >=(MalNum a, MalNum b)
+            {
+                return a.myVal >= b.myVal;
+            }
+            public static bool operator <=(MalNum a, MalNum b)
+            {
+                return a.myVal <= b.myVal;
+            }
+            public static bool operator >(MalNum a, MalNum b)
+            {
+                return a.myVal > b.myVal;
+            }
+            public static bool operator <(MalNum a, MalNum b)
+            {
+                return a.myVal < b.myVal;
+            }
+        }
+
+        // Derived class for Mal nil
+        public class MalNil : MalVal
+        {
+            public override string ToString(bool printReadably)
+            {
+                return "nil";
+            }
+        }
+
+        // Derived class for Mal false
+        public class MalFalse : MalVal
+        {
+            public override string ToString(bool printReadably)
+            {
+                return "false";
+            }
+        }
+
+        // Derived class for Mal true
+        public class MalTrue : MalVal
+        {
+            public override string ToString(bool printReadably)
+            {
+                return "true";
+            }
+        }
+
+        // Derived class for Mal strings.
+        public class MalString : MalVal
+        {
+            private string MyValue;
+
+            public MalString(string str)
+            {
+                MyValue = str;
+            }
+
+            public override string ToString()
+            {
+                // Copied from the reference version.
+                return "\"" + MyValue + "\"";
+            }
+
+            public override string ToString(bool print_readably)
+            {
+                // Varies from the reference version which mixes up strings and keywords.
+                if (print_readably)
+                {
+                    return "\"" + MyValue.Replace("\\", "\\\\")
+                        .Replace("\"", "\\\"")
+                        .Replace("\n", "\\n") + "\"";
+                }
+                else
+                {
+                    return MyValue;
+                }
+            }
+        }
+
+        // Derived class for Mal keywords. The C#-Mal Ref uses annnotated strings instead.
+        public class MalKeyword : MalVal
+        {
+            private string MyKeyword;
+
+            public MalKeyword(string str)
+            {
+                MyKeyword = str;
+            }
+
+            public override string ToString()
+            {
+                return MyKeyword;
+            }
+
+            public override string ToString(bool printReadably)
+            {
+                return MyKeyword;
+            }
+        }
+
+        public class MalQuote : MalVal
+        {
+            // Doesn't have its own value.
+            public override string ToString(bool printReadably)
+            {
+                return "'";
+            }
+        }
+
+        // Derived class to hold Mal functions, both core and created by Def*. I admit,
+        // I looked at C#-Mal to get this right.
+        public class MalFunc : MalVal
+        {
+            // From C#-Mal
+            Func<MalList, MalVal> fn = null;
+            MalVal ast = null;
+            Env env = null;
+            MalList fparams = null;
+
+            // This says it's a lambda that takes a MalList (its args) and returns a MalVal
+            public MalFunc(Func<MalList, MalVal> fn)
+            {
+                this.fn = fn;
+            }
+
+            public MalFunc(MalVal ast, Env e, MalList fparams, Func<MalList, MalVal> fn)
+            {
+                this.ast = ast;
+                this.env = e;
+                this.fparams = fparams;
+                this.fn = fn;
+            }
+
+            public override string ToString(bool printReadably)
+            {
+                if (ast != null)
+                {
+                    return "<fn " + Printer.pr_str(fparams, true) +
+                           " " + Printer.pr_str(ast, true) + ">";
+                }
+                else
+                {
+                    return "<builtin_function " + fn.ToString() + ">";
+                }
+            }
+
+            // Call the stored func, passing it a list containing args, if any.
+            public MalVal Apply(MalList args)
+            {
+                return fn(args);
+            }
+        }
+    }
+}
