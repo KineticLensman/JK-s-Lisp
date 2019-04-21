e0b422fc477a4abf9e8584066b6cab40ee1161ac
KineticLensman <john.kent.42@googlemail.com>
2019-04-21 15:43:59 +0100

    Add files via upload

---
+++ Mal-step-9/ReadLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+namespace Mal
+{
+    // A seperate class for consistency with the Mal guide.
+    static public class ReadLine
+    {
+        public static string Readline(string prompt)
+        {
+            Console.Write(prompt);
+            Console.Out.Flush();
+            return Console.ReadLine();
+        }
+    }
+}
