35da34fc101622852dbe09986a9fa660c7fac4af
KineticLensman <john.kent.42@googlemail.com>
2020-04-12 21:18:29 +0100

    Add files via upload

---
+++ ReadLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JKLApp
+{
+    // A seperate class for consistency with the Mal guide.
+    static public class ReadLine
+    {
+        public static string Readline(string prompt)
+        {
+            Console.Write(prompt);
+            Console.Out.Flush();
+            return Console.ReadLine();
+        }
+    }
+}
